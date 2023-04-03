baf279a5dfbb6d23f2821d70b21fe6d037881309
Khai Do <64216725+KrisKai@users.noreply.github.com>
2023-04-03 17:40:41 +0700

    update id

--- JourneySick.Data/Models/DTOs/TripDTO.cs
+++ JourneySick.Data/Models/DTOs/TripDTO.cs
@@ -8,7 +8,7 @@ namespace JourneySick.Data.Models.DTOs
 {
     public class TripDTO
     {
-        public string FldTripId { get; set; }
+        public string? FldTripId { get; set; }
 
         public string? FldTripName { get; set; }
 
