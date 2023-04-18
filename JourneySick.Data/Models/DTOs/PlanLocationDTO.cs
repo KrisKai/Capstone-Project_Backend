99ffcecab7b5d4b5a4993ecbafd5bf7f37efc2b3
Khai Do <64216725+KrisKai@users.noreply.github.com>
2023-04-19 01:03:05 +0700

    Update PlanLocationDTO.cs

--- JourneySick.Data/Models/DTOs/PlanLocationDTO.cs
+++ JourneySick.Data/Models/DTOs/PlanLocationDTO.cs
@@ -9,23 +9,13 @@ namespace JourneySick.Data.Models.DTOs
     public class PlanLocationDTO
     {
         public int? FldPlanId { get; set; }
-
-        public int? FldOrdinal { get; set; }
-
         public string? FldPlanLocationId { get; set; }
-
-        public string? FldPlanLocationName { get; set; }
-
+        public int? FldMapId { get; set; }
         public string? FldPlanLocationDescription { get; set; }
-
         public DateTime? FldLocationArrivalTime { get; set; }
-
         public DateTime? FldCreateDate { get; set; }
-
         public string? FldCreateBy { get; set; }
-
         public DateTime? FldUpdateDate { get; set; }
-
         public string? FldUpdateBy { get; set; }
     }
 }
