b15fcb75cd77ef546ca05c49fd6fc8319e7f071b
Khai Do <dooanhkhai@outlook.com>
2023-05-12 10:45:07 +0700

    Create TripStatisticResponse.cs

---
+++ JourneySick.Data/Models/DTOs/CommonDTO/TripStatisticResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace JourneySick.Data.Models.DTOs.CommonDTO
+{
+	public class TripStatisticResponse
+	{
+		public int tripCountThisYear;
+        public int tripCountThisMonth;
+        public string trendStatus;
+        public int countDiff;
+    }
+}
+
