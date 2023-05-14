1c37ae320642c20edd5291dbdf8baa7afd18788c
PhatPT <phamtanphat0901@outlook.com>
2023-05-14 17:29:52 +0700

    Email Service

---
+++ JourneySick.Business/Extensions/Email/EmailService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using System.IO;
+
+namespace RevenueSharingInvest.Business.Services.Extensions.Email
+{
+    public static class EmailService
+    {
+        //private static readonly string App_Password = "hchr lwct gcor qtsr";
+        private static readonly string APP_PASSWORD = "cxrdzurzsizgzaep";
+        private static readonly string SENDER = "journeysick.noreply@gmail.com";
+        public static async Task SendEmail(string filePath, string receiver, string sender)
+        {
+            string SendMailSubject = "JourneySick - Invitation To Another Journey!!!";
+            string SendMailBody = "ok";
+
+            try
+            {
+                SmtpClient SmtpServer = new("smtp.gmail.com", 587)
+                {
+                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                    Timeout = 5000,
+                    EnableSsl = true,
+                    UseDefaultCredentials = false,
+                    Credentials = new NetworkCredential(SENDER, APP_PASSWORD)
+                };
+
+                MailMessage email = new()
+                {
+                    // START
+                    From = new MailAddress(SENDER),
+                    Subject = SendMailSubject,
+                    Body = SendMailBody
+                };
+
+                email.To.Add(receiver);
+                email.CC.Add(SENDER);
+
+                //END
+
+                SmtpServer.Send(email);
+                email.Attachments.ToList().ForEach(x => x.Dispose());
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+
+        }
+    }
+}
