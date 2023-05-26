59eebdbfe2ddb20b47a9a99bfdbc39d1218de4c4
Khai Do <64216725+KrisKai@users.noreply.github.com>
2023-05-26 21:33:19 +0700

    Update ItemRequest.cs

--- JourneySick.Data/Models/DTOs/CommonDTO/Request/ItemRequest.cs
+++ JourneySick.Data/Models/DTOs/CommonDTO/Request/ItemRequest.cs
@@ -14,6 +14,7 @@ namespace JourneySick.Data.Models.DTOs.CommonDTO.Request
         public int CategoryId { get; set; }
         public decimal? PriceMax { get; set; }
         public decimal? PriceMin { get; set; }
+        public int? Quantity { get; set; }
 
     }
 
